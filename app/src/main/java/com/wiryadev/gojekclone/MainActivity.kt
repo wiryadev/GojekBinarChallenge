f8032d6d75f9998266ec1d3463dc84e6894eb189
wiryadev <abrarwiryawan@gmail.com>
2022-02-28 22:23:26 +0700

    Initial commit

---
+++ app/src/main/java/com/wiryadev/gojekclone/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.wiryadev.gojekclone
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.wiryadev.gojekclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/wiryadev/gojekclone/ui/order/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.wiryadev.gojekclone.ui.order
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
